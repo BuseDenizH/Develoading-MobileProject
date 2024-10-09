67472fe45a94f5e238ca5fd206705aa95c5dfede
Taha Alparslan Atabey <121360091+tatabey@users.noreply.github.com>
2024-10-09 20:59:30 +0300

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'photo-gallery',
-  webDir: 'dist'
-};
-
-export default config;
