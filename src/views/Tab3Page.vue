4773e53363c2270ef6b5a24f805f9551f858a85a
Ahmet Eren Arslan <erosmus1220@gmail.com>
2024-10-14 21:06:02 +0300

    implement page 3 first design

--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 3</ion-title>
+        <ion-title>Kartlar</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
@@ -12,7 +12,59 @@
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 3 page" />
+      <ion-searchbar 
+        placeholder="Kart ara..." 
+        showCancelButton="true" 
+        debounce="300"
+      ></ion-searchbar>
+
+      <div style="text-align: left; margin-bottom: 10px; padding-left: 20px;">
+        <p style="text-decoration: underline;">Kayıtlı Kartlar</p>
+      </div>
+
+      <div style="display: flex; flex-wrap: wrap; justify-content: flex-start; gap: 10px;">
+        <ion-card style="width: 30%; background-color: transparent;">
+          <ion-card-content>
+            <img src="/Users/eren/Desktop/a.png" alt="Açıklayıcı fotoğraf" style="width: 100%; height: auto;" />
+          </ion-card-content>
+          <ion-button fill="clear">Axess</ion-button>
+        </ion-card>
+
+        <ion-card style="width: 30%; background-color: transparent;">
+          <ion-card-content>
+            <img src="/Users/eren/Desktop/bonus.png" alt="Açıklayıcı fotoğraf" style="width: 100%; height: auto;" />
+          </ion-card-content>
+          <ion-button fill="clear">Bonus</ion-button>
+        </ion-card>
+      </div>
+
+      <div style="text-align: left; margin-bottom: 10px; padding-left: 20px;">
+        <p style="text-decoration: underline;">Kartlar</p>
+      </div>
+
+      <div style="display: flex; flex-wrap: wrap; justify-content: flex-start; gap: 10px;">
+        <ion-card style="width: 30%; background-color: transparent;">
+          <ion-card-content>
+            <img src="/Users/eren/Desktop/maximum_card_logo.png" alt="Açıklayıcı fotoğraf" style="width: 100%; height: auto;" />
+          </ion-card-content>
+          <ion-button fill="clear">Maximum</ion-button>
+        </ion-card>
+
+        <ion-card style="width: 30%; background-color: transparent;">
+          <ion-card-content>
+            <img src="/Users/eren/Desktop/world_card_logo.png" alt="Açıklayıcı fotoğraf" style="width: 100%; height: auto;" />
+          </ion-card-content>
+          <ion-button fill="clear">World</ion-button>
+        </ion-card>
+
+        <ion-card style="width: 30%; background-color: transparent;">
+          <ion-card-content>
+            <img src="/Users/eren/Desktop/icon.webp" alt="Açıklayıcı fotoğraf" style="width: 100%; height: auto;" />
+          </ion-card-content>
+          <ion-button fill="clear">Hopi</ion-button>
+        </ion-card>
+      </div>
+      
     </ion-content>
   </ion-page>
 </template>
