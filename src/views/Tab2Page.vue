5ae72897fecd19c7c5ccdeb4ba58152e75e89c16
tatabey <tatabey02@gmail.com>
2024-10-15 19:52:17 +0300

    Companies Page(Tab2Page) added

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -2,74 +2,94 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Deneme</ion-title>
+        <ion-buttons slot="start">
+          <ion-button @click="goBack" class="back-button">
+            <ion-icon aria-hidden="true" :icon="arrowBackOutline" />
+            <ion-label>Geri</ion-label>
+          </ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div class="container">
-        <img src="https://www.theinsuranceemporium.co.uk/blog/wp-content/uploads/2023/09/image-10.png" alt="horse">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores totam officiis doloremque quibusdam aliquid? Quisquam.</p>
-      </div>
-      <div class="container">
-        <img src="https://b1157417.smushcdn.com/1157417/wp-content/uploads/2024/07/cow-close-up-of-dairy-cattle-825x550.jpg?lossy=1&strip=1&webp=0" alt="cow">
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia, ratione eum. Iste quisquam tempora blanditiis.</p>
-      </div>
-      <div class="container">
-        <img src="https://media.4-paws.org/c/7/2/e/c72ed88142663ae07df3a3d7a1f9a0b831e15f0c/VIER_PFOTEN_2020-10-24_00191-3000x1569-1200x628.jpg" alt="sheep">
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia, ratione eum. Iste quisquam tempora blanditiis.</p>
-      </div>
-      <div class="container">
-        <img src="https://www.theinsuranceemporium.co.uk/blog/wp-content/uploads/2023/09/image-10.png" alt="horse">
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia, ratione eum. Iste quisquam tempora blanditiis.</p>
-      </div><div class="container">
-        <img src="https://www.theinsuranceemporium.co.uk/blog/wp-content/uploads/2023/09/image-10.png" alt="horse">
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia, ratione eum. Iste quisquam tempora blanditiis.</p>
-      </div>
-
+      <ion-searchbar v-model="searchTerm" placeholder="Ara..." style="margin-top:20px"></ion-searchbar>
+      <ion-grid>
+        <ion-row>
+          <ion-col v-for="item in filteredItems" :key="item.id" size="6">
+            <div>
+              <img :src="item.image" @click="handleClick(item.id)" style="width: 100%; height: 100px; margin-top:10px; cursor: pointer;"/>
+              <div class="image-caption" @click="handleClick(item.id)">
+                <h3>{{ item.title }}</h3>
+              </div>
+            </div>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
+
+
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { ref, computed } from 'vue';
+import {  IonContent, IonCol, IonGrid, IonPage, IonRow,IonSearchbar, IonHeader, IonToolbar, IonButtons, IonButton, IonIcon} from '@ionic/vue';
+import { arrowBackOutline } from 'ionicons/icons';
+
+
+import resim1 from '@/assets/resim1.jpg';
+import resim2 from '@/assets/resim2.jpg';
+import resim3 from '@/assets/resim3.jpeg';
+import resim4 from '@/assets/resim4.jpg';
+import resim5 from '@/assets/resim5.jpeg';
+import resim6 from '@/assets/resim6.jpeg';
+import resim7 from '@/assets/resim7.jpeg';
+
+const items = ref([
+  { id: 1, title: 'Akaryakıt', image: resim1 },
+  { id: 2, title: 'Giyim/Kozmetik', image: resim2 },
+  { id: 3, title: 'Elektronik', image: resim3 },
+  { id: 4, title: 'Yeme/İçme', image: resim4 },
+  { id: 5, title: 'Turizm/Seyahat', image: resim5 },
+  { id: 6, title: 'Telekomünikasyon', image: resim6 },
+  { id: 7, title: 'Araç Kiralama', image: resim7 }
+]);
+
+const searchTerm = ref('');
+
+const filteredItems = computed(() => {
+  const term = searchTerm.value.toLowerCase();
+  if (!term) {
+    return items.value;
+  }
+  return items.value.filter(item => 
+    item.title.toLowerCase().includes(term)
+  );
+});
+
+const handleClick = (imageId: number) => {
+  console.log(`Resim ${imageId} tıklandı!`);
+};
 </script>
 
+
+
 <style scoped>
-.container {
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  flex-direction: column;
-  text-align: center;
-  padding-top: 16px;
-  margin-top: 16px;
-  margin-left: 16px;
-  margin-right: 16px;
-  border-radius: 10px;
-  background-color: aqua;
+.back-button {
+  color: blue; 
 }
 
-.container img {
-  width: 300px;
-  height: auto;
-  border-radius: 10px;
+.image-caption {
+  text-align: center; 
+  margin-top: 1px; 
+  border: 1px solid #ccc; 
+  border-radius: 5px; 
+  padding: 5px; 
+  background-color: #f8f9fa; 
+  cursor: pointer; 
 }
 
-.container p {
-  font-size: 16px;
-  line-height: 22px;
-  color: white;
-  background-color: blueviolet;
-  margin: 10px;
-  width: 100%;
-  padding: 5px 16px;
-  font-weight: bold;
+.image-caption h3 {
+  font-size: 1.0em; 
+  margin: 0; 
 }
 </style>
