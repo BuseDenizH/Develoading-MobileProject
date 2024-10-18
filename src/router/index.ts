c7b881670145e6a0577e24049d21a814a6d42799
Funda Çelik <fundacelik@Funda-MacBook-Pro.local>
2024-10-18 11:52:02 +0300

    Tab dosya isimlerini güncelledim

--- src/router/index.ts
+++ src/router/index.ts
@@ -33,19 +33,19 @@ const routes: Array<RouteRecordRaw> = [
       },
       {
         path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
+        component: () => import('@/views/Home.vue')
       },
       {
         path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
+        component: () => import('@/views/Companies.vue')
       },
       {
         path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
+        component: () => import('@/views/Cards.vue')
       },
       {
         path: 'tab4',
-        component: () => import('@/views/Tab4Page.vue')
+        component: () => import('@/views/Profile.vue')
       }
     ]
   }
